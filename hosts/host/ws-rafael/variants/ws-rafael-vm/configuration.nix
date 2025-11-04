296d18d2fa9fb8311cead6a2da5664b9aec182da
Rafael Brandão <rafa.bra.dev@pm.me>
2025-11-04 03:18:38 -0300

    Home - 2025.11.03

--- hosts/host/ws-rafael/variants/ws-rafael-vm/configuration.nix
+++ hosts/host/ws-rafael/variants/ws-rafael-vm/configuration.nix
@@ -1,4 +1,36 @@
 _: {
+  boot.initrd = {
+    clevis = {
+      enable = true;
+      useTang = true;
+      devices = {
+        "cryptroot".secretFile = "/etc/clevis/cryptroot.jwe";
+        "cryptswap".secretFile = "/etc/clevis/cryptswap.jwe";
+      };
+    };
+    network = {
+      enable = true;
+      # udhcpc.enable = true;
+    };
+  };
+  environment.etc = {
+    "clevis/cryptroot.jwe" = {
+      source = ./secrets/cryptroot.jwe;
+      # optional but recommended: restrict permissions and ownership
+      mode = "0400";
+      user = "root";
+      group = "root";
+    };
+    "clevis/cryptswap.jwe" = {
+      source = ./secrets/cryptswap.jwe;
+      mode = "0400";
+      user = "root";
+      group = "root";
+    };
+  };
+  security.sudo = {
+    wheelNeedsPassword = false;
+  };
   services = {
     spice-autorandr.enable = true;
     spice-vdagentd.enable = true;
