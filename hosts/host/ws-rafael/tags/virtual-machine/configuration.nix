95c9f9e0b0a6afc08a7250dd7fa8d0ee804b4b7e
Rafael Brandão <rafa.bra.dev@pm.me>
2026-04-27 15:45:43 -0300

    fix(vm): enable systemd initrd to restore clevis/tang auto-unlock at boot

--- hosts/host/ws-rafael/tags/virtual-machine/configuration.nix
+++ hosts/host/ws-rafael/tags/virtual-machine/configuration.nix
@@ -1,13 +1,28 @@
 {
   boot = {
     initrd = {
-      network.enable = true;
+      network = {
+        enable = true;
+        flushBeforeStage2 = true;
+      };
+
       clevis = {
         enable = true;
         useTang = true;
         devices = {
           "cryptroot".secretFile = "/etc/clevis/cryptroot.jwe";
           "cryptswap".secretFile = "/etc/clevis/cryptswap.jwe";
+        };
+      };
+      systemd = {
+        enable = true;
+        emergencyAccess = true;
+        network = {
+          enable = true;
+          networks."10-eth" = {
+            matchConfig.Type = "ether";
+            networkConfig.DHCP = "ipv4";
+          };
         };
       };
     };
