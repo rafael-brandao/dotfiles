e03b6c75257e17e40598ec3badae6fcbd97132a8
Rafael Brandão <rafa.bra.dev@pm.me>
2026-04-28 22:45:32 -0300

    fix(fish): fix keybinding condition using test instead of [

--- users/user/rafael/home/core/programs/shell/fish.nix
+++ users/user/rafael/home/core/programs/shell/fish.nix
@@ -43,28 +43,8 @@ with lib; {
             fish_vi_key_bindings
           end
 
-          # Functions needed for !! and !$
-          function __history_previous_command
-              switch (commandline -t)
-              case "!"
-                commandline -t $history[1]; commandline -f repaint
-              case "*"
-                commandline -i !
-              end
-          end
-
-          function __history_previous_command_arguments
-              switch (commandline -t)
-              case "!"
-                commandline -t ""
-                commandline -f history-token-search-backward
-              case "*"
-                commandline -i '$'
-              end
-          end
-
           # The bindings for !! and !$
-          if [ $fish_key_bindings = fish_vi_key_bindings ];
+          if test "$fish_key_bindings" = fish_vi_key_bindings
               bind -Minsert ! __history_previous_command
               bind -Minsert '$' __history_previous_command_arguments
           else
