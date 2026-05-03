085bca3cc075053981e3e747d4f2a067e66c5933
Rafael Brandão <rafa.bra.dev@pm.me>
2026-05-03 02:05:48 -0300

    feat(u:rafael): configure MangoWM

--- users/user/rafael/home/core/services/window-managers/mangowc.nix
+++ users/user/rafael/home/core/services/window-managers/mangowc.nix
@@ -14,10 +14,84 @@ in
           echo 'Starting MangoWC'
         '';
       settings = {
+        # Binds
         bind = [
-          "SUPER,r,reload_config"
+          "SUPER+SHIFT,r,reload_config"
           "SUPER,Return,spawn,ghostty"
+          "SUPER,q,killclient,"
         ];
+
+        # Effects
+        blur = 0;
+        blur_layer = 1;
+        blur_optimized = 1;
+        blur_params = {
+          brightness = 0.9;
+          contrast = 0.9;
+          noise = 0.02;
+          num_passes = 2;
+          radius = 5;
+          saturation = 1.2;
+        };
+
+        shadows = 1;
+        layer_shadows = 1;
+        shadow_only_floating = 1;
+        shadows_blur = 15;
+        shadows_size = 12;
+        shadows_position = {
+          x = 0;
+          y = 0;
+        };
+        shadowscolor = "0x000000ff";
+
+        border_radius = 6;
+        no_radius_when_single = 0;
+        focused_opacity = 1.0;
+        unfocused_opacity = 0.85;
+
+        # Animation Configuration
+        animations = 1;
+        animation = {
+          curve = {
+            close = "0.08,0.92,0,1";
+            focus = "0.46,1.0,0.29,1";
+            move = "0.46,1.0,0.29,1";
+            opafadein = "0.46,1.0,0.29,1";
+            opafadeout = "0.58,0.98,0.58,0.98";
+            open = "0.46,1.0,0.29,1.1";
+            tag = "0.46,1.0,0.29,1";
+          };
+          duration = {
+            close = 800;
+            focus = 400;
+            move = 500;
+            open = 400;
+            tag = 350;
+          };
+          fade = {
+            "in" = 1;
+            out = 1;
+          };
+          type = {
+            close = "slide";
+            open = "zoom";
+          };
+        };
+        fadein_begin_opacity = 0.6;
+        fadeout_begin_opacity = 0.8;
+        layer_animations = 1;
+        layer_animation = {
+          type = {
+            close = "slide";
+            open = "slide";
+          };
+        };
+        tag_animation_direction = 1;
+        zoom = {
+          initial_ratio = 0.4;
+          end_ratio = 0.7;
+        };
       };
     };
 
