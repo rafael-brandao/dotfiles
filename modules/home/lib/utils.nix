0a68e83e73d02f288d948ba064f9f57b335985b8
Rafael Brandão <rafa.bra.dev@pm.me>
2026-05-26 12:10:34 -0300

    feat(home-manager): add systemd exec argument escaping helpers to lib.utils
    Copied `escapeSystemdExecArg` and `escapeSystemdExecArgs` from
    `nixpkgs/nixos/lib/utils.nix`.
    
    These helpers allow safely quoting arguments for ExecStart*/ExecStop*
    lines, with proper handling of special characters (%, $), JSON-style
    quoting, and type coercion.

--- modules/home/lib/utils.nix
+++ modules/home/lib/utils.nix
@@ -1,5 +1,24 @@
-{config, ...}: {
-  config.lib.utils = {
+{
+  config,
+  lib,
+  ...
+}: let
+  inherit
+    (builtins)
+    toJSON
+    ;
+  inherit
+    (lib)
+    isPath
+    isString
+    isInt
+    isFloat
+    isDerivation
+    replaceStrings
+    concatMapStringsSep
+    ;
+in {
+  config.lib.utils = rec {
     restartSystemdService = serviceName: let
       systemctl = config.systemd.user.systemctlPath;
     in
@@ -15,5 +34,28 @@
 
         unset systemdStatus
       '';
+
+    # Quotes an argument for use in Exec* service lines.
+    # systemd accepts "-quoted strings with escape sequences, toJSON produces
+    # a subset of these.
+    # Additionally we escape % to disallow expansion of % specifiers. Any lone ;
+    # in the input will be turned it ";" and thus lose its special meaning.
+    # Every $ is escaped to $$, this makes it unnecessary to disable environment
+    # substitution for the directive.
+    escapeSystemdExecArg = arg: let
+      s =
+        if isPath arg
+        then "${arg}"
+        else if isString arg
+        then arg
+        else if isInt arg || isFloat arg || isDerivation arg
+        then toString arg
+        else throw "escapeSystemdExecArg only allows strings, paths, numbers and derivations";
+    in
+      replaceStrings ["%" "$"] ["%%" "$$"] (toJSON s);
+
+    # Quotes a list of arguments into a single string for use in a Exec*
+    # line.
+    escapeSystemdExecArgs = concatMapStringsSep " " escapeSystemdExecArg;
   };
 }
